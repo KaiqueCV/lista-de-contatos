7f5ab2b33cab846a328b9f035b92d59bdaeb3b79
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-31 13:11:41 -0300

    style: adicionado novas variaveis

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,9 @@ export const Colors = {
   blue: '#f0f8ff',
   blue50: '#add8e6',
   blue100: '#4169e1',
-  blue150: '#4682b4'
+  blue150: '#4682b4',
+  yellow: '#FFD700',
+  danger: '#c53a21'
 }
 
 export const Gradients = {
