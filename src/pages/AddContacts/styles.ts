067f0ccb2b683a9ef39ea3c56a431940340559d5
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-31 08:56:43 -0300

    style: adicionado variaveis para colors e estilos css

---
+++ src/pages/AddContacts/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Colors } from '../../styles/theme'
+
+export const ContainerAddContact = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 16px 24px;
+  background: ${Colors.blue};
+`
---
+++ src/pages/MyContacts/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Colors } from '../../styles/theme'
+
+export const ContainerMyContact = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 16px;
+  background: ${Colors.blue};
+`
